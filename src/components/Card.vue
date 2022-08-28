8e75ad5b6fedef1dbb001a44224017b5396d4295
joo <vividjtj@gmail.com>
2022-08-28 23:46:33 +0900

    사진 게시물 업로드

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,43 +1,50 @@
 <template>
-  <div class="col">
-    <div class="card shadow-sm">
-      <svg
-        class="bd-placeholder-img card-img-top"
-        width="100%"
-        height="225"
-        xmlns="http://www.w3.org/2000/svg"
-        role="img"
-        aria-label="Placeholder: Thumbnail"
-        preserveAspectRatio="xMidYMid slice"
-        focusable="false"
-      >
-        <title>Placeholder</title>
-        <rect width="100%" height="100%" fill="#55595c" />
-        <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
-      </svg>
+  <div class="card shadow-sm">
+    <span class="img" :style="{ backgroundImage: `url(${item.imgPath})` }" />
+    <div class="card-body">
+      <p class="card-text">
+        {{ item.name }}
+        <small class="discount badge bg-danger text-muted"
+          >{{ item.discount }}%</small
+        >
+      </p>
+      <div class="d-flex justify-content-between align-items-center">
+        <button class="btn btn-primary">구입하기</button>
+        <small class="price text-muted"
+          >{{ lib.getNumberFormatted(item.price) }}원</small
+        >
 
-      <div class="card-body">
-        <p class="card-text">{{ item }}</p>
-        <div class="d-flex justify-content-between align-items-center">
-          <div class="btn-group">
-            <button type="button" class="btn btn-sm btn-outline-secondary">
-              View
-            </button>
-            <button type="button" class="btn btn-sm btn-outline-secondary">
-              Edit
-            </button>
-          </div>
-          <small class="text-muted">9 mins</small>
-        </div>
+        <small class="real text-danger"
+          >{{
+            lib.getNumberFormatted((item.price * (100 - item.discount)) / 100)
+          }}원</small
+        >
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import lib from "@/scripts/lib";
+
 export default {
-  props: { item: String },
+  props: { item: Object },
+  setup() {
+    return { lib };
+  },
 };
 </script>
 
-<style></style>
+<style scoped>
+.card .img {
+  display: inline-block;
+  width: 100%;
+  height: 250px;
+  background-size: cover;
+  background-position: center;
+}
+
+.card .card-body .price {
+  text-decoration: line-through;
+}
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,28 +1,10 @@
 <template>
   <div class="home">
-    <section class="py-5 text-center container">
-      <div class="row py-lg-5">
-        <div class="col-lg-6 col-md-8 mx-auto">
-          <h1 class="fw-light">Album example</h1>
-          <p class="lead text-muted">
-            Something short and leading about the collection below—its contents,
-            the creator, etc. Make it short and sweet, but not too short so
-            folks don’t simply skip over it entirely.
-          </p>
-          <p>
-            <a href="#" class="btn btn-primary my-2">Main call to action</a>
-            <a href="#" class="btn btn-secondary my-2">Secondary action</a>
-          </p>
-        </div>
-      </div>
-    </section>
-
     <div class="album py-5 bg-light">
       <div class="container">
         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
           <div v-for="(i, index) in state.items" :key="index">
-            Card
-            {{ i }}
+            {{ i.id }}
             <!-- 부모(현재 Home.vue)컴포넌트에서 자식(현재 Card.vue) 컴포넌트로 데이터를 바인딩 시켜 넘겨 줄 때, :변수명=데이터 해당 표시로 넘겨 준다. 그리고 자식 컴포넌트에 props: {변수명: 자료형} 으로 받아 준다. -->
             <Card :item="i"></Card>
           </div>
@@ -48,6 +30,7 @@ export default {
 
     axios.get("/api/items").then(({ data }) => {
       state.items = data;
+      console.log(data);
     });
     return { state };
   },
