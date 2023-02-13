62059702406e873486b09eb9b5f69179ed0d7a75
주태윤 <vividjtj@gmail.com>
2023-02-13 13:22:54 +0900

    TodoList 컴포넌트,

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,7 +1,14 @@
 <template>
-  <div>
-    <input type="text" v-model="newTodoItem" />
-    <button @click="addTodo">추가</button>
+  <div class="inputBox shadow">
+    <input
+      type="text"
+      v-model="newTodoItem"
+      placeholder="Type What you have to do"
+      @keyup.enter="addTodo"
+    />
+    <span class="addContainer" @click="addTodo">
+      <i class="addBtn fas fa-plus" aria-hidden="true"></i>
+    </span>
   </div>
 </template>
 
@@ -17,11 +24,39 @@ export default {
       if (this.newTodoItem !== "" && this.newTodoItem !== undefined) {
         const value = this.newTodoItem && this.newTodoItem.trim();
         window.localStorage.setItem(this.newTodoItem, this.newTodoItem);
-        this.newTodoItem = "";
+        this.clearInput();
       }
+    },
+    clearInput() {
+      this.newTodoItem = "";
     },
   },
 };
 </script>
 
-<style></style>
+<style scope>
+input:focus {
+  outline: none;
+}
+.inputBox {
+  background: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+.inputBox input {
+  border-style: none;
+  font-size: 0.9rem;
+}
+.addContainer {
+  float: right;
+  background: linear-gradient(to right, #6478fb, #8763fb);
+  display: block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+}
+.addBtn {
+  color: white;
+  vertical-align: middle;
+}
+</style>
